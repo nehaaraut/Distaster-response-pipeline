69b79523590130313712de77ae8a39dc6b86227e
Neha Raut <71001920+nehaaraut@users.noreply.github.com>
2020-11-13 14:42:37 +0530

    Add files via upload

---
+++ app/run.py
@@ -0,0 +1,114 @@
+import json
+import plotly
+import pandas as pd
+import pickle
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+import joblib
+from sqlalchemy import create_engine
+
+
+# import warnings filter
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
+app = Flask(__name__)
+
+def tokenize(text):
+    # normalize text and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # tokenize text
+    tokens = word_tokenize(text)
+    stop_words = stopwords.words("english")
+    words = [w for w in tokens if w not in stop_words]
+    
+    # Reduce words to their stems
+    stemmer = PorterStemmer()
+    stemmed = [stemmer.stem(w) for w in words]
+    
+    # Reduce words to their root form
+    lemmatizer = WordNetLemmatizer()
+    lemmed = [lemmatizer.lemmatize(w) for w in stemmed]
+    
+    return lemmed
+
+# load data
+engine = create_engine('sqlite:///C:/Users/Nehaa/project/data/messages_categories.db')
+df = pd.read_sql_table('messages_categories', engine)
+
+# load model
+filename = 'C:/Users/Nehaa/project/models/random_forest_best.pkl'
+model = joblib.load(filename)
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        }
+    ]
+    
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=8000, debug=True)
+
+
+if __name__ == '__main__':
+    main()
